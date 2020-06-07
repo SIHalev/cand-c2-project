560d834af4c3b6141e4bfea1fb9ef77333708f65
Stoyan Halev <sto@foryouandyourcustomers.com>
2020-06-07 21:25:21 +0300

    Udacity Project "Design, Provision and Monitor AWS Infrastructure at Scale"

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,41 @@
-# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block = "10.1.0.0/16"
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+  tags = {
+    Name = "Udacity terraform vpc"
+  }
+}
 
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  cidr_block = "10.1.10.0/24"
+  map_public_ip_on_launch = true
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+  tags = {
+    Name = "Udacity terraform subnet"
+  }
+}
+
+resource "aws_instance" "Udacity_T2" {
+  count = 4
+  ami = "ami-01d025118d8e760db"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public_subnet.id
+  tags = {
+    Name = "Udacity terraform T2 instance"
+  }
+}
+
+resource "aws_instance" "Udacity_M4" {
+  count = 2
+  ami = "ami-01d025118d8e760db"
+  instance_type = "m4.large"
+  subnet_id = aws_subnet.public_subnet.id
+  tags = {
+    Name = "Udacity terraform M4 instance"
+  }
+}
